f554fc95fdd9e6cbbfdba6917aa842590a4f56a0
uday172 <udayreddy569@gmail.com>
2024-07-18 15:35:48 +0530

    Array Task Commited

---
+++ javapratice/src/Arrays/OddEvenPositionArray.java
@@ -0,0 +1,48 @@
+package Arrays;
+import java.util.*;
+public class OddEvenPositionArray {
+	
+	public static void swap(int a , int b, int arr[]) {
+		int t = arr[a];
+		arr[a] = arr[b];
+		arr[b] = t;
+	}
+
+	public static void main(String[] args) {
+		int n;
+		System.out.print("Enter the size of Array :");
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		System.out.println("Enter the elements of an array :");
+		int arr[] = new int[n];
+		for(int i=0;i<n;i++) {
+			arr[i] = sc.nextInt();
+		}
+		int l = 0;
+		int h = 0;
+		while(h<n) {
+			if(arr[h]%2==0 && l%2==0 && arr[h]!=0) {
+				if(l!=h) {
+					swap(l,h,arr);
+					l++;
+				}
+			}
+			else if(arr[h]%2!=0 && l%2!=0){
+				if(l!=h) {
+					swap(l,h,arr);
+					l++;	
+				}
+			}
+			else {
+				h++;
+			}
+		}
+		while(l<n) {
+			arr[l++]=0;
+		}
+		for(int i=0;i<n;i++) {
+			System.out.print(arr[i]+" ");
+		}
+	}
+
+}
